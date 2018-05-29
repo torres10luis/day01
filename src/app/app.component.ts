5cd128d1bdc2eafbe3b50ae578bc69b0bb660e98
torres10luis <Luis.angel_15@yahoo.com>
2018-05-29 16:52:10 -0700

    typescript practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  yetAnotherStr; // type undefined
+  anotherStr: string;
   title = 'my first website';
+
+  str: string;
+
+
+  ngOnInit() {
+    this.str = 'this is a 4';
+
+    this.anotherStr = 'this is srting';
+
+    this.yetAnotherStr = 4;
+
+    this.test();
+
+    this.numberArrayInput([]);
+    this.numberArrayInput([5]);
+    this.numberArrayInput([5, 10, 15]);
+
+    this.anyArrayInput([]);
+    this.anyArrayInput([5]);
+    this.anyArrayInput([5, 10, 15]);
+    this.anyArrayInput(['string',5]);
+
+  }
+
+  test() {
+    let name: string = 'test user';
+    let age: number = 30;
+    let text: string = `My name is ${name} and i am ${age} years old`;
+    console.log(text);
+  }
+  numberArrayInput(x: number[]) {
+    console.log(x);
+  }
+  anyArrayInput(x: Array<any>) {
+    console.log(x);
+  }
 }
